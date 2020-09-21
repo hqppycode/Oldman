9c6d8aabaa6427054a25ce499c91d0890bd1a750
Richard <richardwalls649@gmail.com>
2020-09-21 09:30:15 -0500

    vercel

--- pages/[id].js
+++ pages/[id].js
@@ -1,37 +1,37 @@
-// import fire from "../config/fire-config";
-// import Link from "next/link";
-// const ReactMarkdown = require("react-markdown");
+import fire from "../config/fire-config";
+import Link from "next/link";
+const ReactMarkdown = require("react-markdown");
 
-// export default function Blog(props) {
-//   return (
-//     <div>
-//       <h1>{props.title}</h1>
-//       <ReactMarkdown className="spacing" source={props.content} />
-//       <Link href="/">
-//         <a className="back-button">Back</a>
-//       </Link>
-//     </div>
-//   );
-// }
+export default function Blog(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <ReactMarkdown className="spacing" source={props.content} />
+      <Link href="/">
+        <a className="back-button">Back</a>
+      </Link>
+    </div>
+  );
+}
 
-// export const getServerSideProps = async ({ query }) => {
-//   const content = {};
-//   await fire
-//     .firestore()
-//     .collection("blog")
-//     .doc(query.id)
-//     .get()
-//     .then((result) => {
-//       content["title"] = result.data().title;
-//       content["content"] = result.data().content;
-//     });
-//   return {
-//     props: {
-//       title: content.title,
-//       content: content.content,
-//     },
-//   };
-// };
+export const getServerSideProps = async ({ query }) => {
+  const content = {};
+  await fire
+    .firestore()
+    .collection("blog")
+    .doc(query.id)
+    .get()
+    .then((result) => {
+      content["title"] = result.data().title;
+      content["content"] = result.data().content;
+    });
+  return {
+    props: {
+      title: content.title,
+      content: content.content,
+    },
+  };
+};
 
 // export async function getStaticPaths() {
 //   const paths = await fire
@@ -49,6 +49,6 @@
 //   };
 // }
 
-export default function blah() {
-  return <h1>blog</h1>;
-}
+// export default function blah() {
+//   return <h1>blog</h1>;
+// }
