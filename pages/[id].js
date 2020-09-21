1879c02c23103b08248cddd60781d2ceadebf478
Richard <richardwalls649@gmail.com>
2020-09-21 07:37:28 -0500

    err10

--- pages/[id].js
+++ pages/[id].js
@@ -1,76 +1,37 @@
-import fire from "../config/fire-config";
-import Link from "next/link";
-const ReactMarkdown = require("react-markdown");
+// import fire from "../../config/fire-config";
+// import Link from "next/link";
+// const ReactMarkdown = require("react-markdown");
 
-export default function Blog({ content }) {
-  return (
-    <div>
-      <h1>{content.title}</h1>
-      <ReactMarkdown className="spacing" source={content.content} />
-      <Link href="/">
-        <a className="back-button">Back</a>
-      </Link>
-    </div>
-  );
-}
-
-// export async function getStaticPaths() {
-//   const paths = await fire
-//     .firestore()
-//     .collection("blog")
-//     .onSnapshot((snap) => {
-//       snap.docs.map((doc) => {
-//         doc.id;
-//       });
-//     });
-//   console.log(paths);
-//   return {
-//     paths: paths,
-//     fallback: false,
-//   };
+// export default function Blog(props) {
+//   return (
+//     <div>
+//       <h1>{props.title}</h1>
+//       <ReactMarkdown className="spacing" source={props.content} />
+//       <Link href="/">
+//         <a className="back-button">Back</a>
+//       </Link>
+//     </div>
+//   );
 // }
 
-// export const getStaticProps = async ({ query }) => {
-//   const blog = {};
+// export const getServerSideProps = async ({ query }) => {
+//   const content = {};
 //   await fire
 //     .firestore()
 //     .collection("blog")
 //     .doc(query.id)
 //     .get()
 //     .then((result) => {
-//       blog["title"] = result.data().title;
-//       blog["content"] = result.data().content;
+//       content["title"] = result.data().title;
+//       content["content"] = result.data().content;
 //     });
-
 //   return {
 //     props: {
-//       blog,
+//       title: content.title,
+//       content: content.content,
 //     },
 //   };
 // };
-
-// This works in local developement
-
-export const getServerSideProps = async ({ query }) => {
-  const content = {};
-  await fire
-    .firestore()
-    .collection("blog")
-    .doc(query.id)
-    .get()
-    .then((result) => {
-      content["title"] = result.data().title;
-      content["content"] = result.data().content;
-    });
-  return {
-    props: {
-      title: content.title,
-      content: content.content,
-    },
-  };
-};
-
-// has not been shown to work
 
 // export async function getStaticPaths() {
 //   const paths = await fire
@@ -78,10 +39,10 @@ export const getServerSideProps = async ({ query }) => {
 //     .collection("blog")
 //     .onSnapshot((snap) => {
 //       snap.docs.map((doc) => {
-//         doc.id;
+//         id: doc.id;
 //       });
 //     });
-//   console.log(paths);
+
 //   return {
 //     paths,
 //     fallback: false,
